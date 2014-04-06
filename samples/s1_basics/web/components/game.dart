7376ccec0af2c2ccf1b134ffeb1fcd0b120d9e2b
Guillaume Girou <girou.g@sfeir.com>
2014-04-06 16:37:45 +0200

    Few project files for step 1

---
+++ samples/s1_basics/web/components/game.dart
@@ -0,0 +1,10 @@
+
+import 'package:polymer/polymer.dart';
+
+@CustomTag('risk-game')
+class RiskGame extends PolymerElement {
+  // Whether styles from the document apply to the contents of the component
+  bool get applyAuthorStyles => true;
+
+  RiskGame.created(): super.created();
+}
