cf7de510e9d9c5d84bcac32042398ad60f1457cd
Mathieu Breton <mbreton@xebia.fr>
2014-04-15 15:51:27 +0200

    Update web folder from step 7

--- samples/s9_server/web/players.dart
+++ samples/s9_server/web/players.dart
@@ -4,22 +4,20 @@ import 'package:risk/risk.dart';
 
 @CustomTag('risk-players')
 class RiskPlayers extends PolymerElement {
+  PlayerState player = new PlayerStateImpl(2, "John Lennon", "kadhafi.png", "blue", reinforcement: 2);
+  
   @published
   Iterable<PlayerState> players = [ //
     new PlayerStateImpl(1, "Paul McCartney", "castro.png", "green", reinforcement: 0), //
     new PlayerStateImpl(2, "John Lennon", "kadhafi.png", "blue", reinforcement: 2), //
-    new PlayerStateImpl(3, "Ringo Starr", "staline.png", "yellow", reinforcement: 0), //
-    new PlayerStateImpl(4, "George Harrison", "kim-jong-il.png", "red", reinforcement: 0), //
+    new PlayerStateImpl(3, "Ringo Starr", "staline.png", "yellow", reinforcement: 1), //
+    new PlayerStateImpl(4, "George Harrison", "kim-jong-il.png", "red", reinforcement: 4), //
   ];
 
   @published
   int activePlayerId = 2;
 
-  @published
-  List<int> playersOrder = [3, 4, 2, 1];
-
   RiskPlayers.created(): super.created();
 
-  sort(List<int> playersOrder) => (Iterable<PlayerState> players) => new List.from(players) //
-      ..sort((PlayerState a, PlayerState b) => playersOrder.indexOf(a.playerId).compareTo(playersOrder.indexOf(b.playerId)));
+  String capitalize(String s) => s.toUpperCase();
 }
