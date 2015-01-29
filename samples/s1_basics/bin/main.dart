ca43116bd0cba19f23b874ca036decd1dd88fcee
Drone Build <support@drone.io>
2015-01-29 20:49:43 +0000

    'Step s9_server'

--- samples/s1_basics/bin/main.dart
+++ samples/s1_basics/bin/main.dart
@@ -1,9 +1,67 @@
 library risk.main;
 
-import '../lib/risk.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http_server/http_server.dart' show VirtualDirectory;
+import 'package:risk/risk.dart';
+import 'package:risk_engine/server.dart';
 
-main() {
-  Country country = new Country('eastern_australia', ['western_australia', 'new_guinea', 'eastern_australia']);
-  var neighbours = country.neighbours;
-  print("Hello ${country.id} and $neighbours!");
+const DEFAULT_PORT = 3000;
+const DEFAULT_PATH = '../web';
+
+VirtualDirectory vDir;
+
+main(List<String> args) {
+  int port = args.length > 0 ? int.parse(args[0], onError: (_) => DEFAULT_PORT) : DEFAULT_PORT;
+  String path = Platform.script.resolve(args.length > 1 ? args[1] : DEFAULT_PATH).toFilePath();
+  runZoned(() {
+    HttpServer.bind(InternetAddress.ANY_IP_V4, port).then((server) {
+      print("Risk is running on http://localhost:$port\nBase path: $path");
+      vDir = new VirtualDirectory(path)
+          ..jailRoot = false
+          ..allowDirectoryListing = true
+          ..directoryHandler = directoryHandler;
+      var riskServer = new RiskWsServer();
+      server.listen((HttpRequest req) {
+        if (req.uri.path == '/ws') {
+          WebSocketTransformer.upgrade(req).then(riskServer.handleWebSocket);
+        } else if (req.uri.path == '/new') {
+          riskServer = new RiskWsServer();
+          req.response.redirect(req.uri.resolve('/'));
+        } else {
+          vDir.serveRequest(req);
+        }
+      });
+    });
+  }, onError: (e) => print("An error occurred $e"));
+}
+
+void directoryHandler(dir, request) {
+  final indexUri = new Uri.file(dir.path).resolve('index.html');
+  vDir.serveFile(new File(indexUri.toFilePath()), request);
+}
+
+class RiskWsServer extends AbstractRiskWsServer {
+  final RiskGameEngine engine;
+
+  Codec<Object, Map> get engineEventCodec => EVENT;
+
+  RiskWsServer() : this.fromEngine(new RiskGameEngine(new StreamController.broadcast(), new RiskGameStateImpl()));
+  RiskWsServer.fromEngine(this.engine);
+
+  void listen(Stream ws, int playerId) {
+    // Decode JSON
+    ws.map(JSON.decode)
+    // Log incoming events
+    .map(logEvent("IN", playerId))
+    // Decode events
+    .map(EVENT.decode)
+    // Avoid unknown events and cheaters
+    .where((event) => event is PlayerEvent && event.playerId == playerId)
+    // Handle events in game engine
+    .listen(engine.handle)
+    // Connection closed
+    .onDone(() => print("Player $playerId left"));
+  }
 }
